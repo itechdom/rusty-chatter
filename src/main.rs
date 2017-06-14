1227040546ef7aa3ac147b1ea145d996c9bdfe7d
Sam Alghanmi <oalghnmi@gmail.com>
2017-06-14 12:44:50 -0400

    main rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate mio;
+use mio::*;
+use std::net::SocketAddr;
+use mio::tcp::*;
+
+struct WebSocketServer {
+    socket: TcpListener,
+    clients: HashMap<Token, TcpStream>,
+    token_counter: usize
+}
+
+impl Handler for WebSocketServer {
+    // Traits can have useful default implementations, so in fact the handler
+    // interface requires us to provide only two things: concrete types for
+    // timeouts and messages.
+    // We're not ready to cover these fancy details, and we wouldn't get to them
+    // anytime soon, so let's get along with the defaults from the mio examples:
+    type Timeout = usize;
+    type Message = ();
+
+}
+
 fn main() {
+    let mut event_loop = EventLoop::new().unwrap();
+    let mut handler = WebSocketServer;
+    event_loop.run(&mut handler).unwrap();
+
+    let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
+    let server_socket = TcpListener::bind(&address).unwrap();
+    event_loop.register(&server_socket,
+                    Token(0),
+                    EventSet::readable(),
+                    PollOpt::edge()).unwrap();
+
     println!("Hello, world!");
 }
---
+++ target/debug/deps/rusty_chatter-699916c2566e604a.d
@@ -0,0 +1,5 @@
+/Users/osamaalghanmi/rusty-chatter/target/debug/deps/rusty_chatter-699916c2566e604a: src/main.rs
+
+/Users/osamaalghanmi/rusty-chatter/target/debug/deps/rusty_chatter-699916c2566e604a.d: src/main.rs
+
+src/main.rs:
