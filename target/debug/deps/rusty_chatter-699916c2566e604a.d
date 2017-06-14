9d7984335993514020087e396bc38362448eca03
Sam Alghanmi <oalghnmi@gmail.com>
2017-06-14 12:45:50 -0400

    target

--- target/debug/deps/rusty_chatter-699916c2566e604a.d
+++
@@ -1,5 +0,0 @@
-/Users/osamaalghanmi/rusty-chatter/target/debug/deps/rusty_chatter-699916c2566e604a: src/main.rs
-
-/Users/osamaalghanmi/rusty-chatter/target/debug/deps/rusty_chatter-699916c2566e604a.d: src/main.rs
-
-src/main.rs:
